a833b06a696ae865e090301fb044a5c86092b1e0
Brett Piatek <brett.piatek@g.austincc.edu>
2016-12-01 16:31:59 -0600

    Push11.30
    Mission Successful

---
+++ Lab8/Date.hpp
@@ -0,0 +1,69 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <string>
+
+using namespace std;
+
+enum DateFormat { SHORT_DATE, LONG_DATE };
+
+class Date {
+private:
+  // Member variables
+  int month, day, year;
+  // Member functions
+  void setDefaultDate();
+public:
+  // Constructors
+  Date() {
+    setDefaultDate();
+  };
+  Date(int m, int d, int y) {
+    if (isValidDate(m, d, y) == false)
+      setDefaultDate();
+    else {
+      month = m; day = d; year = y;
+    }
+  }
+  // Destructors
+  ~Date() {}
+  // Getters
+  int getMonth() { return month; }
+  int getDay() { return day; }
+  int getYear() { return year; }
+  // Setter function prototypes
+  bool setMonth(int m);
+  bool setDay(int d);
+  bool setYear(int y);
+  // Member function prototypes
+  bool isDefaultDate();
+  bool setDate(int m, int d, int y);
+  string toString(DateFormat df = SHORT_DATE);
+  static bool isValidDate(int m, int d, int y);
+  static bool isLeapYear(int y);
+
+  //Arithmetic Operator Overloads
+
+  friend bool operator<(Date a, Date b);
+  friend bool operator>(Date a, Date b);
+  friend bool operator==(Date a, Date b);
+  friend bool operator<=(Date a, Date b);
+  friend bool operator>=(Date a, Date b);
+  
+  //Prefix and postfix increment operator overload
+  
+  Date operator++(); 
+  
+  //Prefix and postfix decrement operator overload
+  
+  Date operator--(); 
+  
+  int getDayinYear(int, int, int); 
+
+};
+
+static bool isValidDate(int m, int d, int y);
+static bool isLeapYear(int y);
+static int getDaysinMonth(int m, int y);
+
+#endif
---
+++ Lab8/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Date.hpp"
+using namespace std; 
+int getDayinYear(int, int, int); 
+
+bool getDate(int& month, int& day, int& year) {
+  string input, token;
+  int mdy[] = { 0,0,0 };
+
+  cout << "Date (mm/dd/yyyy)? ";
+  getline(cin, input);
+
+  stringstream ss(input);
+  int index = 0;
+  while (getline(ss, token, '/') && index < 3) {
+    istringstream iss(token);
+    iss >> mdy[index++];
+  }
+  month = mdy[0]; day = mdy[1]; year = mdy[2];
+  return (input == "") ? false : true;
+}
+
+int main (){
+	Date x; 
+	Date daysAfter; 
+	Date daysBefore; 
+	Date daysInMonth; 
+	int month, day, year; 
+	cout<<"Welcome to the day converter and leap year calculator!\n"; 
+	while (true){
+		getDate(month, day, year);
+  			if (Date::isValidDate(month, day, year) == true) {
+   			x.setDate(month, day, year); 
+    		cout << "The date is " << x.toString(SHORT_DATE);
+    		cout << " (" << x.toString(LONG_DATE) << "). ";
+    		string s = Date::isLeapYear(x.getYear()) == true ? "is" : "is not";
+    		cout << "This " << s << " a leap year." << endl;
+    		cout<<"This is day #"<<getDayinYear(month, day, year)<<" of the year."<<endl; 
+  			}
+  		else {
+    		cout << "The entry is not a valid date." << endl;
+    		break;
+  		}
+		  /*		
+  		daysAfter = x; 
+  		
+  		daysAfter.operator++();
+  	
+  		cout <<  "The next day is: "<<daysAfter.toString(SHORT_DATE)<<endl; 
+  		
+  		daysAfter.operator++(); 
+  		 
+  		cout<<"The day after is: "<<daysAfter.toString(SHORT_DATE)<<endl;
+  		
+  		daysBefore= x; 
+  		
+  		daysBefore.operator--();
+  	
+  		cout <<  "The day before is: "<<daysBefore.toString(SHORT_DATE)<<endl; 
+  		
+  		daysBefore.operator--(); 
+  		 
+  		cout<<"The day before that is: "<<daysBefore.toString(SHORT_DATE)<<endl;*/	
+	}
+}
